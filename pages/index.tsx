29f159c601f2ba1424be1f2f51a636f779299041
wh0sumit <sumikumar1608@gmail.com>
2022-07-27 06:49:22 +0530

    Initial Commit : Demo Website

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,45 +5,39 @@ import {
   VStack,
   Box,
   Center,
-  HStack,
   Button,
   useColorModeValue,
   Stack,
-  IconButton,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
-
-import * as RiIcons from "react-icons/ri";
 
 const Home: NextPage = () => {
   return (
     <Container maxW={"5xl"} p="12">
       <VStack spacing="2" paddingTop={"5rem"} alignItems="flex-start">
-        <Heading as="h1" pb={"5"} size="2xl">
+        <Heading as="h1" pb={"3"} size="2xl">
           Welcome to DunoLabs !
         </Heading>
-        <Text as="p" fontSize="lg">
+        <Text as="p" fontSize="lg" pb={"5"}>
           DunoLabs' mission is to develop open-source micro-saas applications
           using various technologies to help businesses & individuals. we cover
           all the aspects by providing a wonderful cross-platform user
           experience.
         </Text>
-        <br />
-        <Heading as="h1" pb={"5"} textDecor="underline">
+        <Heading as="h1" pb={"3"} textDecor="underline">
           About
         </Heading>
-        <Text as="p" fontSize="lg">
+        <Text as="p" fontSize="lg" pb={"5"}>
           Founded in April 2022, DunoLabs' plan is to develop micro-saas
           applications across multiple domains. DunoLabs teams up a community of
           experts to develop open-source software that enables users to
           accomplish tasks quickly and easily. The main goal is to provide a
           phenomenal user experience.
         </Text>{" "}
-        <br />
         <Heading as="h1" pb={"5"} textDecor="underline">
           Work
         </Heading>
-        <Center py={6}>
+        <Center pb={"5"}>
           <Box
             maxW={"445px"}
             w={"full"}
@@ -51,7 +45,6 @@ const Home: NextPage = () => {
             boxShadow={"md"}
             rounded={"xl"}
             p={6}
-            overflow={"hidden"}
           >
             <Stack>
               <Text
