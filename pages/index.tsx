466670e604ed53edaa88a5fb29f5a6caef2e746e
wh0sumit <sumikumar1608@gmail.com>
2023-03-03 18:54:33 +0530

    Demo Website Updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,90 +8,88 @@ import {
   Button,
   useColorModeValue,
   Stack,
+  Link,
+  Divider,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Container maxW={"5xl"} p="12">
+    <Container maxW={"4xl"} p="12">
       <VStack spacing="2" paddingTop={"5rem"} alignItems="flex-start">
-        <Heading as="h1" pb={"3"} size="2xl">
-          Welcome to DunoLabs !
-        </Heading>
-        <Text as="p" fontSize="lg" pb={"5"}>
-          DunoLabs mission is to develop open-source micro-saas applications
-          using various technologies to help businesses & individuals. we cover
-          all the aspects by providing a wonderful cross-platform user
-          experience.
-        </Text>
-        <Heading as="h1" pb={"3"} textDecor="underline">
-          About
-        </Heading>
-        <Text as="p" fontSize="lg" pb={"5"}>
-          Founded in April 2022, DunoLabs plan is to develop micro-saas
-          applications across multiple domains. DunoLabs teams up a community of
-          experts to develop open-source software that enables users to
-          accomplish tasks quickly and easily. The main goal is to provide a
-          phenomenal user experience.
-        </Text>{" "}
-        <Heading as="h1" pb={"5"} textDecor="underline">
-          Work
-        </Heading>
-        <Center pb={"5"}>
-          <Box
-            maxW={"445px"}
-            w={"full"}
-            bg={useColorModeValue("white", "gray.900")}
-            boxShadow={"md"}
-            rounded={"xl"}
-            p={6}
-          >
-            <Stack>
-              <Text
-                color={"purple.300"}
-                fontWeight={"bold"}
-                textDecor="underline"
-                as="a"
-                href="https://invoicetor.works"
-              >
-                Invoicetor
-              </Text>
-
-              <Text color={"gray.500"}>
-                Invoicetor is a no-code platform where business owners can
-                create invoices for their business in no time. Invoicetor is a
-                platform that helps businesses to digitize their finances in an
-                easy manner and create invoices in a simple way.
-              </Text>
-            </Stack>
-            <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
-              <Button
-                as={"a"}
-                href="https://github.com/dunolabs/invoicetor-landing"
-                target={"_blank"}
-              >
-                Github
-              </Button>{" "}
-              <Button
-                as={"a"}
-                href="https://invoicetor.works"
-                target={"_blank"}
-              >
-                Visit
-              </Button>
-            </Stack>
-          </Box>
-        </Center>
-        <Stack padding={"3"} mt={"5rem"} bg="yellow.100" rounded={"xl"}>
-          <Text fontWeight={"500"}>
-            This is a demo website for the Dunolabs. we&apos;re currently
-            working on our main website.
+        <Box
+          as="a"
+          href="https://wh0sumit.notion.site/work-dunolabs-e0dbc9782fda45208ed9ab23d9227dc7"
+          target={"_blank"}
+          bg="purple.50"
+          rounded={"full"}
+          px={"3"}
+          py={"1"}
+          border="1px"
+          borderColor="purple.300"
+          color={"purple.600"}
+          transition="all 0.3s ease"
+        >
+          <Text fontSize="sm" fontWeight={"medium"}>
+            we are hiring 👀
           </Text>
-        </Stack>
+        </Box>
+        <Heading as="h2" fontWeight={"bold"}>
+          welcome to dunolabs.
+        </Heading>
+        <Divider />
+        <Box maxW={"3xl"} textAlign={"left"} pb={"5"}>
+          <Text fontSize="md" fontWeight={"medium"}>
+            dunoLabs is a friendly, product-service-based company that's here to
+            help you build your business and product from the ground up! we
+            won't only help you build your brand (product) but also market it –
+            wink wink 😉
+          </Text>
+          <Text fontSize="md" pt={"3"} fontWeight={"medium"}>
+            we're a team that is well-versed in all aspects of product design,
+            development, and growth. we focus on creating an engaging user
+            experience that is tailored to your specific needs 🙌
+          </Text>
+          <Text fontSize="md" pt={"3"} fontWeight={"medium"}>
+            plus, we offer a broad range of services from web and mobile app
+            development to marketing and support. with dunolabs, you can have
+            peace of mind knowing that your product is being built, marketed,
+            and maintained with the utmost care 💎
+          </Text>
+        </Box>
+        <Text fontSize="md" fontWeight={"medium"}>
+          job application link :{" "}
+          <Link
+            href="https://wh0sumit.notion.site/work-dunolabs-e0dbc9782fda45208ed9ab23d9227dc7"
+            target={"_blank"}
+            color={useColorModeValue("purple.500", "blue.200")}
+          >
+            apply now 💼
+          </Link>
+        </Text>
+
+        <Box maxW={"3xl"} textAlign={"left"} pt="5">
+          <Heading as="h5" size="md" my="2" fontWeight={"bold"}>
+            friendly note to our visitors
+          </Heading>
+          <Divider my="2" />
+          <Text fontWeight={"500"}>
+            this is a demo page for the dunoLabs website. we're currently
+            working on the website and will be launching it soon. in the
+            meantime, you can check out our social media pages and contact us{" "}
+            <Link
+              href="mailto:dunolabs@gmail.com"
+              color={useColorModeValue("purple.500", "blue.200")}
+              textDecoration="underline"
+            >
+              dunolabs@gmail.com
+            </Link>{" "}
+            for more information 🙌
+          </Text>
+        </Box>
       </VStack>
     </Container>
   );
 };
 
 export default Home;
-
