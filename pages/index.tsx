d78ee3d6f47350bd6b0637a9b5e5dd3630f09f27
wh0sumit <sumikumar1608@gmail.com>
2023-04-01 01:57:45 +0530

    feat: Added Initial Setup

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-extrabold text-center text-gray-800 font-satoshi">
+        Home
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
+
