030b07ad87142f3cbd4c60436c72c3c6cb21d08e
Yash Sehgal <yashsehgal.work@gmail.com>
2023-06-25 06:45:21 +0530

    feat(wip): about page layout

---
+++ app/about/page.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layouts";
+import { cn } from "@/lib/cn";
+
+const AboutPage: React.FC = () => {
+  return (
+    <Layout>
+      <div className={cn("py-24")}>
+        About Page
+      </div>
+    </Layout>
+  )
+};
+
+export default AboutPage;
