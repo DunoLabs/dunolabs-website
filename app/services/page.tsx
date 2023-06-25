fb4a08f9c394088933f6c4d686c69225f87de3e6
Yash Sehgal <yashsehgal.work@gmail.com>
2023-06-25 06:48:17 +0530

    feat(wip): services page layout

---
+++ app/services/page.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layouts";
+import { cn } from "@/lib/cn";
+
+const ServicesPage: React.FC = () => {
+  return (
+    <Layout>
+      <div className={cn("py-24")}>
+        Services Page
+      </div>
+    </Layout>
+  )
+};
+
+export default ServicesPage;
