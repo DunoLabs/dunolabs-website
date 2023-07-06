0e2cbca31322779e38fd0882b3f04ebd3ef6aa91
Yash Sehgal <yashsehgal.work@gmail.com>
2023-07-06 14:26:28 +0530

    feat: gh-pages config added

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  assetPrefix: './',
   images: {
     domains: ['github.com'],
+    loader: 'akamai',
+    path: '',
   },
   experimental: {
     appDir: true,
