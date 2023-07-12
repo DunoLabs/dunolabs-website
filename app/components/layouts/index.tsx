dad5afe3dab175161d3ad41f3aaf59e7290d8cd1
Yash Sehgal <yashsehgal.work@gmail.com>
2023-07-12 09:03:27 +0530

    feat: adding mobile-navigation functionality / toggling animation

--- app/components/layouts/index.tsx
+++ app/components/layouts/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@/components/layouts/header';
 import MobileNav from '@/components/ui/mobile-nav';
+import MobileNavigation from '@/components/ui/mobile-nav';
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -11,7 +12,22 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div>
       <Header />
       {children}
-      <MobileNav />
+      <MobileNavigation menuTitle="menu" menuActions={[
+        {
+          menuHeading: "about",
+          menuHeadingURL: "/about",
+          menSubOptions: [
+            { subOptionHeading: "who we are", subMenuOptionURL: "/about#who-we-are" },
+            { subOptionHeading: "work culture", subMenuOptionURL: "/about#work-culture" },
+            { subOptionHeading: "dunolabs website BTS", subMenuOptionURL: "/about#dunolabs-website-bts" }
+          ]
+        },
+        {
+          menuHeading: "people",
+          menuHeadingURL: "/people",
+          menSubOptions: []
+        }
+      ]} />
     </div>
   );
 };
--- app/components/ui/mobile-nav/index.tsx
+++ app/components/ui/mobile-nav/index.tsx
@@ -1,64 +1,85 @@
-import { FC } from 'react';
-import Link from 'next/link';
-import {
-  Menubar,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarTrigger,
-} from '@/components/ui/menu-bar';
-import { Url } from 'url';
+"use client";
 
-interface MenuItem {
-  title: string;
-  href?: string | Url;
-  description?: string;
-}
+import { cn } from "@/lib/cn";
+import { Menu as HamburgerMenuIcon } from "lucide-react";
+import { ButtonHTMLAttributes, MouseEventHandler, useState } from "react";
+import Button from "../buttons";
 
-interface MenuCategory {
-  title: string;
-  href?: string | URL | undefined;
-  submenu?: MenuItem[];
-  state?: string;
-}
-
-const menu: MenuCategory[] = [
-  {
-    title: 'home.',
-    href: '/',
-  },
-  {
-    title: 'about.',
-    href: '/about',
-  },
-  {
-    title: 'people.',
-    href: '/people',
-  },
-  {
-    title: 'contact.',
-    href: '/contact',
-  },
-];
-
-const MobileNav: FC = () => {
-  return (
-    <div className="flex items-center justify-center ">
-      <div className="fixed p-1 transform -translate-x-1/2 border-2 rounded-xl w-100 backdrop-blur bg-white/60 bottom-4 left-1/2 md:hidden">
-        <Menubar className="gap-3 p-4 border-0 rounded-lg bg-zinc-900 text-zinc-50">
-          {menu.map((item, index) => (
-            <MenubarMenu key={index}>
-              {item.href ? (
-                <MenubarTrigger className="items-center justify-center text-md">
-                  <Link href={item.href}>{item.title}</Link>
-                  <MenubarSeparator />
-                </MenubarTrigger>
-              ) : null}
-            </MenubarMenu>
-          ))}
-        </Menubar>
-      </div>
-    </div>
-  );
+interface MobileNavigationProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  menuTitle: string;
+  menuActions: Array<{
+    menuHeading: string | React.ReactNode;
+    menuHeadingURL?: string;
+    menSubOptions: Array<{
+      subOptionHeading: string | React.ReactNode;
+      subMenuOptionURL: string;
+    }>;
+  }>;
 };
 
-export default MobileNav;
+const MobileNavigation: React.FC<MobileNavigationProps> = ({ menuTitle="menu", menuActions, className, ...props }) => {
+  /**
+   * @description state-manager for handling the toggle functionality of the menu 
+   * @type boolean
+   * @nameOfStateValue {isMenuOpen}
+   * @nameOfStateManager {setIsMenuOpen}
+   * @params none
+   * @returns nothing as it's a state-manager function, has to be void
+   * */
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  /**
+   * @description the toggle effect has an issue: when the UI renders,
+   * the conditional animation (on closing the menu-button),
+   * also the "decrease-menu-button-size" CSS runs, 
+   * thus to handle this issue, we're using a interaction-identifier
+   * that can be used to check that the button as been interacted 
+   * once or not -- and apply the animations accordingly
+   * 
+   * @example
+   *  - when the UI renders for the first time, there's gonna be no "decrease-menu-button-size" animation
+   *  - when the user clicks (or interacts) with the menu-button, then the "increase-menu-button-size" animation renders
+   *  - on toggling the mneu-button to be closed, then the "decrease-menu-button-size" animation will render
+   * 
+   * @type: boolean
+   * @nameOfStateValue {hasInteracted}
+   * @nameOfStateManager {setHasInteracted}
+   * @oarams none
+   * @returns nothing as it's a state-manager function, has to be void
+   */
+  const [hasInteracted, setHasInteracted] = useState<boolean>(false);
+
+  /**
+   * @description: handling method to manage the events on clicking the menu-button
+   * @type: void
+   * @nameOfMethod {handleMenuAction}
+   * @params Event / onClick event properties
+   * @paramsType React.MouseEventHandler<HTMLAnchorElement>
+   * @returns: nothing as the type is void
+   */
+  const handleMenuAction = function (e: MouseEventHandler<HTMLAnchorElement> | any): void {
+    // checking if it's the first interaction with the menu-button
+    if (!hasInteracted) setHasInteracted(true);
+    // toggling the current menu state
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <Button 
+      variant={"subtle"}
+      className={cn(
+        "mobile-navigation flex flex-row items-center justify-center gap-2 p-6 border-4 border-zinc-500 transition-all fixed transform -translate-x-1/2 bottom-8 left-1/2 md:hidden", 
+        className, (isMenuOpen ? "increase-menu-button-size": (hasInteracted ? "decrease-menu-button-size": "")))}
+      onClick={handleMenuAction}
+    >
+      <span className="mobile-navigation-menu-title-wrapper text-base">
+        {menuTitle}
+      </span>
+      <span className="mobile-navigation-menu-title-leadingIcon-wrapper text-xl">
+        {"🌱"}
+      </span>
+    </Button>
+  )
+};
+
+export default MobileNavigation;
